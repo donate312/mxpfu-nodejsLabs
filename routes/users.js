17cf9971a41ff180a93c84ca7a5a81089cfc4f97
David <david.onate312@gmail.com>
2023-11-08 12:18:11 -0500

    CRUD ops Node and Express

--- routes/users.js
+++ routes/users.js
@@ -27,22 +27,17 @@ router.get("/",(req,res)=>{
     res.send(JSON.stringify({users},null,4));
 });
 
-// GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
     const email = req.params.email;
     let filtered_users = users.filter((user) => user.email === email);
     res.send(filtered_users);
 });
 
-
-// POST request: Create a new user
 router.post("/",(req,res)=>{
     users.push({"firstName":req.query.firstName,"lastName":req.query.lastName,"email":req.query.email,"DOB":req.query.DOB});
     res.send("The user" + (' ')+ (req.query.firstName) + " Has been added!")
 });
 
-
-// PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
     const email = req.params.email;
     let filtered_users = users.filter((user) => user.email === email);
@@ -74,12 +69,12 @@ router.put("/:email", (req, res) => {
     users = users.filter((user) => user.email != email);
     res.send(`User with the email  ${email} deleted.`);
   });
+
   function getDateFromString(strDate) {
     let [dd,mm,yyyy] = strDate.split('-')
     return new Date(yyyy+"/"+mm+"/"+dd);
 }
     
-// console.log(sorted_users);
 router.get("/sort",(req,res)=>{
     let sorted_users=users.sort(function(a, b) {
         let d1 = getDateFromString(a.DOB);
@@ -88,4 +83,5 @@ router.get("/sort",(req,res)=>{
           });
     res.send(sorted_users);
 });
+
 module.exports=router;
